246ca2482a69b39e2afac99653125cace319df8b
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-03-02 16:07:18 +0000

    adding key to user id on events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_03_02_154221) do
-
+ActiveRecord::Schema.define(version: 2021_03_02_160419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +44,8 @@ ActiveRecord::Schema.define(version: 2021_03_02_154221) do
     t.string "country"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "genres", force: :cascade do |t|
@@ -83,4 +82,5 @@ ActiveRecord::Schema.define(version: 2021_03_02_154221) do
   add_foreign_key "event_attendees", "users"
   add_foreign_key "event_genres", "events"
   add_foreign_key "event_genres", "genres"
+  add_foreign_key "events", "users"
 end
