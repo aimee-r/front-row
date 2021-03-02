84e892fde175fe9d8c77eaff16cd17584b7f10ca
Roberto Piancatelli <piancatellirob@gmail.com>
2021-03-02 11:28:45 +0000

    removed genres column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_173551) do
+ActiveRecord::Schema.define(version: 2021_03_02_112306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,5 @@ ActiveRecord::Schema.define(version: 2021_03_01_173551) do
     t.string "country"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "genres", default: [], array: true
   end
-
 end
