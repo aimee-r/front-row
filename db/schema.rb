64517b2349e86c47afb011a1fdefc3a4b4f62b0c
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-03-02 15:43:41 +0000

    adding user id to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_03_02_153143) do
+ActiveRecord::Schema.define(version: 2021_03_02_154113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_153143) do
     t.string "country"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "genres", force: :cascade do |t|
