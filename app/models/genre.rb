076a2f8dd9f2bc916c10b627331c6ab9aa7d2a0b
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-03-02 18:57:43 +0000

    completing seed file, adding dependent destroy to models

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,5 @@
 class Genre < ApplicationRecord
-    has_many :event_genres
-    has_many :events, through: :event_genres
-    validates :name, presence: true, uniqueness: true
+  has_many :event_genres, dependent: :destroy
+  has_many :events, through: :event_genres, dependent: :destroy
+  validates :name, presence: true, uniqueness: true
 end
