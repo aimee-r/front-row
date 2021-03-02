ced369f5657c8a614a91b19c48b19ad477e919a9
Aimee Rawlings <aimeeclairerawlings@gmail.com>
2021-03-02 17:25:25 +0200

    removed genre seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
-
-50.times do
-  genre = Genre.new(
-    name: Faker::Music.genre
-  )
-  genre.save!
-end
