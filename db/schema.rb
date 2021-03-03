0842aaa8a10e683e6d69a72a5d764c47cf3ef709
Lukas Musilek <lukas.musilek@gmail.com>
2021-03-03 15:04:24 +0000

    Implemented sign up for User and Artist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_160419) do
+ActiveRecord::Schema.define(version: 2021_03_02_162708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_160419) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "nationality"
+    t.string "location"
     t.date "dob"
     t.boolean "artist", default: false
     t.string "artist_name"
