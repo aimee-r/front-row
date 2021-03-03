9d8f0e0bd188d9055d0101318a44f26208678257
Roberto Piancatelli <piancatellirob@gmail.com>
2021-03-03 12:05:27 +0000

    added references to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
+  # REFERENCES
   belongs_to :user
   belongs_to :event
+  # STRIPE
+  monetize :amount_cents
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # REFERENCES
+  has_many :orders
   has_many :events, dependent: :destroy
   has_many :event_attendees
   devise :database_authenticatable, :registerable,
