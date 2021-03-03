b8735dd4789bf5a09cde2f0c701922318074f972
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-03-03 13:27:39 +0000

    updating new event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,7 +20,7 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     @event.user = current_user
-      if @event.save
+      if @event.save!
         redirect_to event_path(@event)
       else
         render :new
