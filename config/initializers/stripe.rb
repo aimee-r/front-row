9a46e25ae703e1238197e33073cfc2f76e246413
Aimee Rawlings <aimeeclairerawlings@gmail.com>
2021-03-04 12:10:27 +0200

    navbar and card adds

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,7 @@
 Rails.configuration.stripe = {
   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-  secret_key:      ENV['STRIPE_SECRET_KEY']
+  secret_key:      ENV['STRIPE_SECRET_KEY'],
   signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY']
-
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
