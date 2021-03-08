46796e0425a41054d83631c3c56e95304b944c77
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-03-08 22:49:51 +0000

    updating chart styles

--- app/services/user_visit.rb
+++ app/services/user_visit.rb
@@ -3,7 +3,7 @@ require "http"
 class UserVisit
   def self.visit(ip, event_id, page)
     ip = ip
-    raise
+    # raise
     response = HTTP.get("https://geolocation-db.com/json/344ec440-6bfc-11eb-a0c0-b5dee9e67313/89.35.196.218")
     hash = JSON.parse(response)
 
