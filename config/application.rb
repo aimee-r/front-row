1cfa0afd5a67bab5832d3f5f1184e38d00c5f8bb
Aimee Rawlings <aimeeclairerawlings@gmail.com>
2021-03-09 18:21:37 +0200

    navbar

--- config/application.rb
+++ config/application.rb
@@ -19,18 +19,11 @@ module FrontRow
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-
     RSpotify::authenticate(ENV['SPOTIFY_KEY'], ENV['SPOTIFY_SECRET_KEY'])
-
-
-    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
-
-    
 end
