dfd32efe0f52afe4aecc5321bc278f71278b945e
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-03-09 22:07:04 +0000

    adding account page styling + global bg & font color + seed update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,19 @@ class UsersController < ApplicationController
 
   def account
     @events = Event.all
+    @event = Event.new
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to users_account_path
+  end
+
+  private
+
+  # PARAMS
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :location, :dob)
   end
 end
