0bf3890a7abc2e751180d7b032e595a749f5cdda
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-03-12 09:20:08 +0000

    adding country and flags to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,7 +57,31 @@ event_name12 = "Dance with me! A Midnight Special"
 event_name13 = "Techno Party X Machina"
 event_name14 = "Lose Yourself To Dance"
 
-event_name_arr = [event_name1, event_name2, event_name3, event_name4, event_name5, event_name6, event_name7, event_name8, event_name9, event_name10, event_name11, event_name12, event_name13, event_name14 ]
+event_name_arr = [event_name1, event_name2,
+
+country1 = "France 🇫🇷"
+country2 = "Streaming From Home | #13"
+country3 = "Jazz Rythyms! Live on Front Row "
+country4 = "Instrumental Orchestra Presents"
+country5 = "Live in London DJ Set"
+country6 = "Streaming From Isolation | #21"
+country7 = "Love To The World on Front Row"
+country8 = "Front Row  x Sugar Mountain Live DJ Set"
+country9 = "Front Row x Amplified"
+country10 = "Sounds of the Underground"
+country11 = "Mellow Moves To Your Living Room"
+country12 = "Dance with me! A Midnight Special"
+country13 = "Techno Party X Machina"
+country14 = "Lose Yourself To Dance"
+country15 = "Front Row x Amplified"
+country16 = "Sounds of the Underground"
+country17 = "Mellow Moves To Your Living Room"
+country18 = "Dance with me! A Midnight Special"
+country19 = "Techno Party X Machina"
+country20 = "Lose Yourself To Dance"
+
+country_arr =[country1, country2, country3, country4, country5, country6, country7, country8, country9, country10, country11, country12, country13, country14, country15, country16, country17, country18, country19, country20]
+
 
 puts "Creating artists..."
 aimee = User.create!(first_name: 'Aimée', last_name: 'Rawlings', location: 'South Africa', dob: '1989-09-14', email: 'aimee@example.com', password: '123456', artist: true, artist_name: 'DJ Amzy', bank_details: '121212 12345678', facebook: 'https://www.facebook.com/daftpunk/', instagram: 'https://www.facebook.com/daftpunk/', youtube: 'https://www.youtube.com/embed/2aFcwLgO1wI',  bio: "DJ Amzy has been at the nexus of music, fashion, and nightlife for over half her living years as the go-to deejay for music impresarios, entertainment moguls, fashion icons, cultural trendsetters, and even world leaders. When President Obama wanted a deejay for both of his Inaugurations and his fiftieth birthday party at the White House, theres only one person he called. When Oprah Winfrey celebrated the opening of her school in South Africa on New Years Eve, there’s only one person she called. And when Jay Z needed a deejay for his wedding to Beyoncé, theres only one person he called." )
